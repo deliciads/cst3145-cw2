c271d4e93c7b3f3afa32381e3ddb6f5696a11a73
deliciads <delicia1999@yahoo.com>
2021-02-14 11:39:23 +0400

    'cw1'

---
+++ lessons.js
@@ -0,0 +1,83 @@
+//lesson subject, location, price, image path and available space stored in javascript file
+let lessons = [
+    {
+        "id": 1001,
+        "title": "Maths",
+        "location": "Dubai",
+        "price": 100.00,
+        "image": "Images/maths.jpg",
+        "availableSpace": 5
+    },
+    {
+        "id": 1002,
+        "title": "English",
+        "location": "Dubai",
+        "price": 75.00,
+        "image": "Images/english.jpg",
+        "availableSpace": 5
+    },
+    {
+        "id": 1003,
+        "title": "Science",
+        "location": "Sharjah",
+        "price": 150.00,
+        "image": "Images/science.jpg",
+        "availableSpace": 5
+    },
+    {
+        "id": 1004,
+        "title": "Business Studies",
+        "location": "Abu Dhabi",
+        "price": 155.00,
+        "image": "Images/business.jpg",
+        "availableSpace": 5
+    },
+    {
+        "id": 1005,
+        "title": "Arabic",
+        "location": "Dubai",
+        "price": 80.00,
+        "image": "Images/arabic.jpg",
+        "availableSpace": 5
+    },
+    {
+        "id": 1006,
+        "title": "Geography",
+        "location": "Dubai",
+        "price": 50.00,
+        "image": "Images/geography.jpg",
+        "availableSpace": 5
+    },
+    {
+        "id": 1007,
+        "title": "Economics",
+        "location": "Ajman",
+        "price": 157.00,
+        "image": "Images/economics.jpg",
+        "availableSpace": 5
+    },
+    {
+        "id": 1008,
+        "title": "Accounting",
+        "location": "Dubai",
+        "price": 100.00,
+        "image": "Images/accounting.jpg",
+        "availableSpace": 5
+    },
+    {
+        "id": 1009,
+        "title": "Psychology",
+        "location": "Ajman",
+        "price": 70.00,
+        "image": "Images/psychology.png",
+        "availableSpace": 5
+    },
+    {
+        "id": 1010,
+        "title": "History",
+        "location": "Sharjah",
+        "price": 46.00,
+        "image": "Images/history.jpg",
+        "availableSpace": 5
+    }
+]
